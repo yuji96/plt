bb7971d07f2f6a8560e1ce096236f37419c0f9c9
yuji96 <y.yama683@gmail.com>
2023-05-21 16:05:04 +0900

    add hbox

--- plt.py
+++ plt.py
@@ -1,13 +1,32 @@
 # flake8: noqa
+import io
 
+import IPython.display as display
+import ipywidgets
 import japanize_matplotlib
+import matplotlib.figure
 import matplotlib.pyplot as plt
 from matplotlib.pyplot import *
 from seaborn import kdeplot
 
 plt.rcParams["figure.facecolor"] = "white"
-plt.rcParams["svg.fonttype"] = "none"
 plt.rcParams["mathtext.fontset"] = "cm"
-plt.rcParams['pdf.fonttype'] = 42
-plt.rcParams["figure.dpi"] = 150
-plt.rcParams['axes.grid'] = True
+plt.rcParams["pdf.fonttype"] = 42
+plt.rcParams["figure.dpi"] = 100
+plt.rcParams["axes.grid"] = True
+
+
+def imshow(*args, **kwargs):
+    im = plt.imshow(*args, **kwargs)
+    plt.gca().grid(False)
+    return im
+
+
+def hbox(figures: list[matplotlib.figure.Figure], **kwargs):
+    kwargs.setdefault("width", 400)
+    widgets = []
+    for fig in figures:
+        buf = io.BytesIO()
+        fig.savefig(buf, format="png")
+        widgets.append(ipywidgets.Image(value=buf.getvalue(), format="png", **kwargs))
+    display.display(ipywidgets.HBox(widgets))
