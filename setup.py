396a090c2364ca868204a572fbf2fd8d47f5eb47
yuji96 <y.yama683@gmail.com>
2023-12-15 02:52:22 +0900

    version 0.2.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="plt",
-    version="0.1.1",
+    version="0.2.0",
     py_modules=["plt"],
     install_requires=Path(__file__)
     .parent.joinpath("requirements.txt")
