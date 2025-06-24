cf80c7a94b4abc7885b671a11b03bb25b4cedafb
yuji96 <y.yama683@gmail.com>
2025-06-24 10:55:13 +0900

    update plt

--- plt.py
+++ plt.py
@@ -4,7 +4,8 @@ import io
 import IPython.display as display
 import ipywidgets
 import matplotlib_fontja
-import matplotlib.figure
+from matplotlib.figure import Figure
+from matplotlib_inline.backend_inline import set_matplotlib_formats
 import matplotlib.pyplot as plt
 from matplotlib import cbook
 from matplotlib.pyplot import *
@@ -16,18 +17,21 @@ except:
     pass
 
 plt.rcParams["axes.grid"] = True
-plt.rcParams["figure.dpi"] = 100
+# plt.rcParams["figure.dpi"] = 100
 plt.rcParams["figure.facecolor"] = "white"
 plt.rcParams["mathtext.fontset"] = "cm"
-plt.rcParams["pdf.fonttype"] = 42
 plt.rcParams["savefig.bbox"] = "tight"
+plt.rcParams["pdf.fonttype"] = 42
+plt.rcParams["font.size"] = 10
+
+set_matplotlib_formats("svg")
 
 
 def detach_tensor(func):
     def wrapper(x, copy=False):
         try:
             if isinstance(x, Tensor):
-                x = x.detach()
+                x = x.detach().cpu()
         except:
             pass
         return func(x, copy=copy)
@@ -35,7 +39,7 @@ def detach_tensor(func):
     return wrapper
 
 
-cbook.safe_masked_invalid = detach_tensor(cbook.safe_masked_invalid)
+cbook._unpack_to_numpy = detach_tensor(cbook._unpack_to_numpy)
 
 
 def imshow(*args, **kwargs):
@@ -78,6 +82,46 @@ def add_colorbar(pad=0.02, width=0.04, im=None, ax=None, fig=None):
 
 def savefig(fname, bbox_inches="tight", pad_inches=0.0, **kwargs):
     return plt.savefig(fname, bbox_inches=bbox_inches, pad_inches=pad_inches, **kwargs)
+
+
+def figure(
+    # autoincrement if None, else integer from 1-N
+    num=None,
+    # defaults to rc figure.figsize
+    figsize=None,
+    # defaults to rc figure.dpi
+    dpi: float | None = None,
+    *,
+    # defaults to rc figure.facecolor
+    facecolor=None,
+    # defaults to rc figure.edgecolor
+    edgecolor=None,
+    frameon: bool = True,
+    FigureClass: type[Figure] = Figure,
+    clear: bool = False,
+    cm=True,
+    w=10,
+    h=4,
+    scale=1.0,
+    **kwargs,
+):
+    if cm:
+        w = w / 2.54  # cm to inch
+        h = h / 2.54  # cm to inch
+    figsize = (w, h) if figsize is None else figsize
+    figsize = tuple([scale * x for x in figsize])
+
+    return plt.figure(
+        num,
+        figsize,
+        dpi,
+        facecolor=facecolor,
+        edgecolor=edgecolor,
+        frameon=frameon,
+        FigureClass=FigureClass,
+        clear=clear,
+        **kwargs,
+    )
 
 
 # def subplots(*args, **kwargs):
